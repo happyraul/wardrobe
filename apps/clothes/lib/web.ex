6d94ff001fefbf5b4579faccdda5aa58122504c4
Raul Taranu <597630+happyraul@users.noreply.github.com>
2020-02-05 19:22:21 +0100

    alternative update item strategy

--- apps/clothes/lib/web.ex
+++ apps/clothes/lib/web.ex
@@ -29,23 +29,40 @@ defmodule Clothes.Web do
     |> Plug.Conn.send_resp(201, "OK")
   end
 
-  post "update_item" do
+  # ALTERNATIVE for /update_item below
+  # def foo(item, term, {:ok, value}) do
+  #   Map.put(item, term, value)
+  # end
+
+  # def foo(item, term, :error) do
+  #   item
+  # end
+
+  # f = fn old_item ->
+  #   old_item
+  #   |> foo(:name, Map.fetch(conn.params, "name"))
+  #   |> foo(:color, Map.fetch(conn.params, "color"))
+  # end
+
+  post "/update_item" do
     conn = Plug.Conn.fetch_query_params(conn)
     user_id = Map.fetch!(conn.params, "user")
     item_id = Map.fetch!(conn.params, "item_id") |> String.to_integer()
 
     f = fn old_item ->
       %{id: item_id}
-      |> (fn new_item -> case Map.fetch(conn.params, "name") do
-        {:ok, name} -> Map.put(new_item, :name, name)
-        :error -> Map.put(new_item, :name, old_item[:name])
-        end
-      end).()
-      |> (fn new_item -> case Map.fetch(conn.params, "color") do
-        {:ok, color} -> Map.put(new_item, :color, color)
-        :error -> Map.put(new_item, :color, old_item[:color])
-        end
-      end).()
+      |> (fn new_item ->
+            case Map.fetch(conn.params, "name") do
+              {:ok, name} -> Map.put(new_item, :name, name)
+              :error -> Map.put(new_item, :name, old_item[:name])
+            end
+          end).()
+      |> (fn new_item ->
+            case Map.fetch(conn.params, "color") do
+              {:ok, color} -> Map.put(new_item, :color, color)
+              :error -> Map.put(new_item, :color, old_item[:color])
+            end
+          end).()
     end
 
     user_id
